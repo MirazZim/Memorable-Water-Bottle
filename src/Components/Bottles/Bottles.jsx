bda922791f055d374847c1f6cd5cbf75270d0b60
MirazZim <miraz.zim.38@gmail.com>
2025-01-02 12:30:30 +0600

    added use effect and loaded data

--- src/Components/Bottles/Bottles.jsx
+++ src/Components/Bottles/Bottles.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+
 const bottles = () => {
+    const [bottles, setBottles] = useState([])
+
+    useEffect( () => {
+        fetch('bottles.json')
+        .then(res => res.json())
+        .then(data => setBottles(data))
+    }
+        ,[])
     return (
         <div>
-            <h3>Bottles Here : </h3>
+            <h3>Bottles Here : {bottles.length} </h3>
         </div>
     );
 };
